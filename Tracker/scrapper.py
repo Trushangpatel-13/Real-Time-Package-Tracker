b24e2e9918a54973d749eb60616327b717a688bc
Trushangpatel-13 <ptrushang@gmail.com>
2021-04-10 01:13:51 +0530

    Address data bug

--- Tracker/scrapper.py
+++ Tracker/scrapper.py
@@ -3,7 +3,7 @@ import time
 import os
 options = webdriver.ChromeOptions()
 options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-#options.add_argument('headless')
+options.add_argument('headless')
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--no-sandbox")
 
